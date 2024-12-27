e505a2c5a108f5a88d4f50b63ea03b4ae4e4d91e
yamadbro97 <nasim.332@gmail.com>
2024-12-28 00:00:28 +0200

    finished Level 6 and 10

--- Assets/scripts/SceneLoader.cs
+++ Assets/scripts/SceneLoader.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NextScene : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     public string LevelName;
     public void LoadScene()
     {
-        SceneManager.LoadScene(LevelName);
+        Debug.Log(SceneManager.sceneCount);
+        if (this.name == "Restart_Button")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+        else
+        {
+            Debug.Log(SceneManager.GetAllScenes().Length);
+
+            SceneManager.LoadScene(LevelName);
+        }
+        Debug.Log(SceneManager.GetActiveScene().name);
     }
 }
